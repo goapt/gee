9a3f24de6fc335d82ca5bc527fb4a9966e0330a5
fifsky <fifsky@gmail.com>
2019-11-21 16:01:35 +0800

    add validator v10

--- go.mod
+++ go.mod
@@ -1,18 +1,26 @@
 module github.com/goapt/gee
 
 require (
+	github.com/cpuguy83/go-md2man/v2 v2.0.0 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/gin-gonic/gin v1.4.0
-	github.com/goapt/golib v1.0.0
+	github.com/go-playground/locales v0.13.0
+	github.com/go-playground/universal-translator v0.17.0
+	github.com/go-playground/validator/v10 v10.0.1
+	github.com/goapt/golib v1.0.3
+	github.com/golang/protobuf v1.3.2 // indirect
 	github.com/google/uuid v1.1.1
 	github.com/ilibs/jsontime v1.0.2
+	github.com/json-iterator/go v1.1.8 // indirect
 	github.com/kr/pretty v0.1.0 // indirect
-	github.com/mattn/go-isatty v0.0.8 // indirect
+	github.com/mattn/go-isatty v0.0.10 // indirect
 	github.com/pkg/errors v0.8.1
-	github.com/urfave/cli v1.20.0
+	github.com/ugorji/go v1.1.7 // indirect
+	github.com/urfave/cli v1.22.1
 	golang.org/x/net v0.0.0-20190611141213-3f473d35a33a // indirect
-	golang.org/x/sys v0.0.0-20190610200419-93c9922d18ae // indirect
+	golang.org/x/sys v0.0.0-20191120155948-bd437916bb0e // indirect
 	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
+	gopkg.in/yaml.v2 v2.2.7 // indirect
 )
 
 go 1.12
---
+++ validator.go
@@ -0,0 +1,70 @@
+package gee
+
+import (
+	"errors"
+	"reflect"
+	"sync"
+
+	"github.com/gin-gonic/gin/binding"
+	"github.com/go-playground/locales/zh"
+	ut "github.com/go-playground/universal-translator"
+	"github.com/go-playground/validator/v10"
+	zhTranslations "github.com/go-playground/validator/v10/translations/zh"
+)
+
+// use a single instance , it caches struct info
+var (
+	uni *ut.UniversalTranslator
+)
+
+type defaultValidator struct {
+	once     sync.Once
+	validate *validator.Validate
+	trans    ut.Translator
+}
+
+var _ binding.StructValidator = &defaultValidator{}
+
+func (v *defaultValidator) ValidateStruct(obj interface{}) error {
+
+	if kindOfData(obj) == reflect.Struct {
+
+		v.lazyinit()
+
+		if err := v.validate.Struct(obj); err != nil {
+			errs := err.(validator.ValidationErrors)
+			terr := errs.Translate(v.trans)
+			for _, v := range terr {
+				return errors.New(v)
+			}
+		}
+	}
+
+	return nil
+}
+
+func (v *defaultValidator) Engine() interface{} {
+	v.lazyinit()
+	return v.validate
+}
+
+func (v *defaultValidator) lazyinit() {
+	v.once.Do(func() {
+		v.validate = validator.New()
+		v.validate.SetTagName("binding")
+		local := zh.New()
+		uni = ut.New(local, local)
+		v.trans, _ = uni.GetTranslator("zh")
+		_ = zhTranslations.RegisterDefaultTranslations(v.validate, v.trans)
+	})
+}
+
+func kindOfData(data interface{}) reflect.Kind {
+	value := reflect.ValueOf(data)
+	valueType := value.Kind()
+
+	if valueType == reflect.Ptr {
+		valueType = value.Elem().Kind()
+	}
+	return valueType
+}
